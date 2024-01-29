c7a0305ab7dd99b7949c0793715abcdaf673f04a
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-01-29 11:24:43 +0600

    task 3 added

---
+++ array-checking.js
@@ -0,0 +1,27 @@
+// Create different variables, each containing either an array or a non-array value.
+
+let myAge = 33;
+let myName = 'Shuvankor Hazra';
+let isDog = false;
+let fruits = ["Mango", "Pineapple", "Orange", "Black barry", "Banana"];
+let animals = ["Cow", "Dog", "Cat", "Goat", "Rabbit"];
+
+console.log(Array.isArray(myAge));
+console.log(Array.isArray(myName));
+console.log(Array.isArray(isDog));
+console.log(Array.isArray(fruits));
+console.log(Array.isArray(animals));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
