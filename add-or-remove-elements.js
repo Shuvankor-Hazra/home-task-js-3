3908565f23ebf020318a63a4ced89f7744639dde
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-01-29 10:44:16 +0600

    task 2 added

---
+++ add-or-remove-elements.js
@@ -0,0 +1,23 @@
+// Declare an array of 3 tourist destinations---
+let touristCentre = [
+  "Sundarbans",
+  "Cox-s-Bazar",
+  "Kuakata",
+  "Shylet",
+  "Bandarban",
+];
+console.log(touristCentre);
+
+// Add a new tourist destination to your tourist array---
+touristCentre.push("Saint Martin");
+console.log(touristCentre);
+
+// Add two more to your array---
+touristCentre.push("Nijhum Deep", "Patenga");
+console.log(touristCentre);
+
+// Remove the last tourist destination you have added
+touristCentre.pop();
+
+// display the final array as output
+console.log(touristCentre);
---
+++ declare-an-array.js
@@ -0,0 +1,29 @@
+// Declare an array with 5 elements containing fruits----
+
+let fruits = ["Mango", "Pineapple", "Orange", "Black barry", "Banana"];
+
+// console log the 3rd index element----
+
+console.log(fruits[3]);
+
+// change the value of the 2nd index element to Jambura----
+
+fruits[2] = "Jambura";
+
+// console log the final array----
+
+console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
