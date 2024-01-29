1e865fd3b05fa94e52d77af26637d8821b2cae04
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-01-29 11:35:06 +0600

    all task added

---
+++ combining-arrays.js
@@ -0,0 +1,12 @@
+// Create two arrays of your choice.
+
+let fruits = ["Mango", "Pineapple", "Orange", "Black barry", "Banana"];
+let animals = ["Cow", "Dog", "Cat", "Goat", "Rabbit"];
+
+// Use the concat method to combine the two arrays into a new array.
+let newMixedArray = fruits.concat(animals);
+
+
+// Print both the original arrays and the combined array using console.log().
+console.log(fruits, animals, newMixedArray);
+
