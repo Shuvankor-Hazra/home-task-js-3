baf0c8a1ee537c12b42b247f257935f0f0714eef
Shuvankr Hazra <shuvo.s.ovuhs@gmail.com>
2024-01-29 11:03:52 +0600

    task 3 added

---
+++ with-includes.js
@@ -0,0 +1,28 @@
+
+// Create an array of books containing different book.
+
+let books = ['HTML book', 'CSS book', 'Tailwind books', 'Daisy-UI book', 'Javascript book'];
+console.log(books);
+
+
+// Use the includes method to check if the array contains a javascript book.
+
+console.log(books.includes('Javascript book'));
+
+
+// Print a message to the console indicating whether the element is present in the array or not.
+
+if (books.includes('Javascript book')){
+    console.log('Now I want to read Javascript books');
+}
+else{
+    console.log('Then I well do the coding');
+}
+
+
+
+
+
+
+
+
